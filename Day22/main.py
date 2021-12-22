8f28194980284e95af87fd9340b2e7c943141922
David <d4v3z02@gmail.com>
2021-12-22 11:38:27 +0100

    ~

--- Day22/main.py
+++ Day22/main.py
@@ -7,16 +7,16 @@ from collections import Counter
 Cube = tuple[bool, CubeGeometry]
 
 
-def parse_cubes() -> list[Cube]:
+def parse_reboot_steps() -> list[Cube]:
     return [
         (line.split()[0] == "on", CubeGeometry.from_line(line))
         for line in read_lines()
     ]
 
 
-def determine_cubes_on(steps: list[Cube], procedure_region=None) -> int:
+def determine_cubes_on_after_reboot(reboot_steps: list[Cube], procedure_region=None) -> int:
     cubes_counter = Counter()
-    for cube_is_on, cube_geometry in steps:
+    for cube_is_on, cube_geometry in reboot_steps:
         if procedure_region and (cube_geometry := cube_geometry.intersect(procedure_region)) is None:
             continue
         new_counter = Counter()
@@ -30,6 +30,6 @@ def determine_cubes_on(steps: list[Cube], procedure_region=None) -> int:
 
 
 if __name__ == '__main__':
-    parsed_cubes = parse_cubes()
-    print_task1(22, determine_cubes_on(parsed_cubes, CubeGeometry(-50, 50, -50, 50, -50, 50)))
-    print_task2(22, determine_cubes_on(parsed_cubes))
+    reboot_steps_parsed = parse_reboot_steps()
+    print_task1(22, determine_cubes_on_after_reboot(reboot_steps_parsed, CubeGeometry(-50, 50, -50, 50, -50, 50)))
+    print_task2(22, determine_cubes_on_after_reboot(reboot_steps_parsed))
