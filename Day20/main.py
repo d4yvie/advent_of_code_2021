e0e42dc62c3766987299bf3b83aab53bc2e89756
David <d4v3z02@gmail.com>
2021-12-23 22:37:16 +0100

    refactoring

--- Day20/main.py
+++ Day20/main.py
@@ -1,6 +1,20 @@
 from file_util import read_lines
 from scipy.signal import convolve2d
+from print_aoc import finish_task1, finish_task2
+from functools import reduce
 import numpy as np
+
+
+EnhanceParameters = tuple[np.array, np.array, np.array]
+
+
+def enhance_n_times(params: EnhanceParameters, times=2):
+    return reduce(enhance, range(times), params)
+
+
+def enhance(params: EnhanceParameters, i: int) -> EnhanceParameters:
+    grid, enhancement, kernel = params
+    return enhancement[convolve2d(grid, kernel, fillvalue=i % 2)], enhancement, kernel
 
 
 if __name__ == '__main__':
@@ -8,8 +22,7 @@ if __name__ == '__main__':
     enhancement = np.array(list(lines[0])) == "#"
     grid = np.array(list(map(list, lines[2:]))) == "#"
     kernel = 2 ** np.arange(9).reshape((3, 3))
-    for i in range(50):
-        # enhance that image! https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.convolve2d.html
-        grid = enhancement[convolve2d(grid, kernel, fillvalue=i % 2)]
-        if i in (1, 49):
-            print(grid.sum())
+    grid, enhancement, kernel = enhance_n_times((grid, enhancement, kernel), 2)
+    finish_task1(20, grid.sum(), 5306)
+    grid, enhancement, kernel = enhance_n_times((grid, enhancement, kernel), 48)
+    finish_task2(20, grid.sum(), 17497)
