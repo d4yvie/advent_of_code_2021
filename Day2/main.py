de691c7d9d5445bac31242f1b7acd1cb04231133
David <d4v3z02@gmail.com>
2021-12-26 14:15:37 +0100

    refactored day2

--- Day2/main.py
+++ Day2/main.py
@@ -1,42 +1,39 @@
 from file_util import read_lines
+from move import Move
+from functools import reduce
+from print_aoc import finish_task1, finish_task2
 
 
-def task1():
-    lines = read_lines()
-    x = 0
-    y = 0
-    for line in lines:
-        line = line.split(" ")
-        move = line[0]
-        severity = int(line[1])
-        if (move == "forward"):
-            x += severity
-        if (move == "up"):
-            y -= severity
-        if (move == "down"):
-            y += severity
-        print(x * y)
+def parse_line(line: str) -> tuple[Move, int]:
+    move, severity = line.split(" ")
+    return Move(move), int(severity)
 
 
-def task2():
-    lines = read_lines()
-    x = 0
-    y = 0
-    aim = 0
-    for line in lines:
-        line = line.split(" ")
-        move = line[0]
-        severity = int(line[1])
-        if (move == "forward"):
-            x += severity
-            y += aim * severity
-        if (move == "up"):
-            aim -= severity
-        if (move == "down"):
-            aim += severity
-        print(x * y)
+calculator_1 = {Move.FORWARD: lambda x, y, severity: (x + severity, y),
+                Move.UP: lambda x, y, severity: (x, y - severity),
+                Move.DOWN: lambda x, y, severity: (x, y + severity)}
+
+
+def calc_position_1(coords: tuple[int, int], line: str) -> tuple[int, int]:
+    x, y = coords
+    move, severity = parse_line(line)
+    return calculator_1[move](x, y, severity)
+
+
+calculator_2 = {Move.FORWARD: lambda x, y, aim, severity: (x + severity, y + aim * severity, aim),
+                Move.UP: lambda x, y, aim, severity: (x, y, aim - severity),
+                Move.DOWN: lambda x, y, aim, severity: (x, y, aim + severity)}
+
+
+def calc_position_2(result: tuple[int, int, int], line: str) -> tuple[int, int, int]:
+    x, y, aim = result
+    move, severity = parse_line(line)
+    return calculator_2[move](x, y, aim, severity)
 
 
 if __name__ == '__main__':
-    task1()
-    task2()
+    lines = read_lines()
+    x, y = reduce(calc_position_1, lines, (0, 0))
+    finish_task1(2, x * y, 1507611)
+    x, y, aim = reduce(calc_position_2, lines, (0, 0, 0))
+    finish_task2(2, x * y, 1880593125)
---
+++ Day2/move.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Move(Enum):
+    FORWARD = "forward"
+    DOWN = "down"
+    UP = "up"
